2f20217e39e6a112fb6c50c618c67f00ef6c92f5
Andrew Leclair <leclair.at@gmail.com>
2021-07-01 23:38:34 -0700

    working on deployable towers

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public int numResources;
+
+    public bool ModifyResourceAmount(int numToModify)
+    {
+        // adding resources
+        if (numToModify > 0)
+        {
+            numResources += numToModify;
+            return true;
+        }
+        // removing resources
+        else if (numResources >= 0 && numToModify < 0)
+        {
+            if (numResources + numToModify < 0)
+            {
+                numResources = 0;
+            } else {
+                numResources += numToModify;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
